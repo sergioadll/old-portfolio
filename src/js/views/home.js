92757d4da480d5063de5dfed2a5c12c303881bef
sergioadll <sergio.adll@hotmail.com>
2020-10-28 16:42:33 +0000

    added animations

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 
 import { Context } from "../store/appContext";
 
+import Button from "react-bootstrap/Button";
+import Typical from "react-typical";
+
 import sergioDiaz from "../../img/sergioDiaz.jpg";
 
 import "../../styles/section.scss";
@@ -25,25 +28,42 @@ export const Home = () => {
 	);
 	return (
 		<>
-			<div id="theme" className="section d-flex justify-content-center align-items-center">
-				<span className="home-title ">SDL</span>
-				<div className="image-container d-flex justify-content-center align-items-center">
-					<img className="profile-image mb-2" src={sergioDiaz} alt="Sergio Diaz Llobera" />
+			<div id="theme" className="section d-flex align-items-center">
+				<div className="d-flex align-items-center ml-5">
+					<span className="home-title ">SDL</span>
+					<div className="image-container d-flex justify-content-center align-items-center">
+						<img className="profile-image mb-2" src={sergioDiaz} alt="Sergio Diaz Llobera" />
+					</div>
+					<div>
+						<Typical
+							steps={["", 1000, "Desarrollador Full-Stack.", 800]}
+							loop={1}
+							wrapper="p"
+							className="home-content "
+						/>{" "}
+						<Typical
+							steps={["", 3000, "Apasionado por los negocios y la tecnología.", 1000]}
+							loop={1}
+							wrapper="p"
+							className="home-content "
+						/>{" "}
+						<Typical
+							steps={["", 6500, "Siempre en proceso de renovación.", 800]}
+							loop={1}
+							wrapper="p"
+							className="home-content "
+						/>
+					</div>
 				</div>
-				<div>
-					<p className="home-content ">Desarrollador Full-Stack. </p>
-					<p className="home-content ">Apasionado por los negocios y la tecnología.</p>
-					<p className="home-content ">Siempre en continua renovación.</p>
+				<div className=" d-flex flex-column ml-auto mr-5">
+					<Button href="/projects" className="section-button my-2" variant="outline-secondary ">
+						Projectos
+					</Button>
+					<Button href="/projects" className="section-button my-2" variant="outline-secondary">
+						Más sobre mí
+					</Button>
 				</div>
 			</div>
 		</>
 	);
 };
-/*
-			<div id="theme" className="section d-flex flex-column justify-content-center align-items-center">
-				<span className="home-title ">SERGIO DIAZ LLOBERA</span>
-				<img className="profile-image mb-2" src={sergioDiaz} alt="Sergio Diaz Llobera" />
-				<p className="home-content ">Desarrollador Full-Stack, apasionado por los negocios.</p>
-				<p className="home-content ">Siempre en continua renovación</p>
-			</div>
-            */
